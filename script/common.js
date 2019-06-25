902c0d5d6e65a8bb2b844b36cfb8d12af4e0dfbe
n1koosta <52212927+n1koosta@users.noreply.github.com>
2019-06-25 22:02:18 +0300

    new

---
+++ script/common.js
@@ -0,0 +1,9 @@
+document.getElementById('menu').onclick = function () {
+    var x = document.getElementById('myTopnav');
+
+    if(x.className === 'topnav') {
+        x.className += ' responsive'
+    } else {
+        x.className = 'topnav';
+    }
+}
